67f1666e563b0c86343194df009d92187d3ad966
Singer Ma <sma@Singers-MacBook-Pro.local>
2015-10-27 12:44:02 -0700

    Adding filtered radia output

--- dnanexus_tools/radia-tool/src/script.sh
+++ dnanexus_tools/radia-tool/src/script.sh
@@ -9,17 +9,66 @@ dx-download-all-inputs --parallel
 mkdir -p out/output_vcf
 mkdir -p out/filtered_output_vcf
 
+if [[ "$patient_id" != "" ]]
+then
+  radia_options="--patientId $patient_id $radia_options"
+  radia_filter_options="--patientId $patient_id $radia_filter_optins"
+fi
+
+if [[ "$fasta_path" == *.gz ]]
+then
+  gunzip "$fasta_path"
+  fasta_path=${fasta_path%.gz}
+  fasta_name=${fasta_name%.gz}
+fi
+
+if [[ "$dnaTumorBai" != "" ]]
+then
+  dnaTumor="--dnaTumorFilename ${dnaTumor_path} --dnaTumorBaiFilename ${dnaTumorBai_path}"
+else
+  dnaTumor="--dnaTumorFilename ${dnaTumor_path}"
+fi
+
+if [[ "$dnaNormalBai" != "" ]]
+then
+  dnaNormal="--dnaNormalFilename ${dnaNormal_path} --dnaNormalBaiFilename ${dnaNormalBai_path}"
+else
+  dnaNormal="--dnaNormalFilename ${dnaNormal_path}"
+fi
+
+if [[ "$rnaTumorBam" != "" ]]
+then
+  if [[ "$rnaTumorBai" != "" ]]
+  then
+    rnaTumor="--rnaTumorFilename ${rnaTumor_path} --rnaTumorBaiFilename ${rnaTumorBai_path}"
+  else
+    rnaTumor="--rnaTumorFilename ${rnaTumor_path}"
+  fi
+fi
+
+if [[ "$rnaNormalBam" != "" ]]
+then
+  if [[ "$rnaNormalBai" != "" ]]
+  then
+    rnaNormal="--rnaNormalFilename ${rnaNormal_path} --rnaNormalBaiFilename ${rnaNormalBai_path}"
+  else
+    rnaNormal="--rnaNormalFilename ${rnaNormal_path}"
+  fi
+fi
+
+
 mkdir radiatemp
 cd radiatemp
 python ~/radia.py \
-  --dnaNormalFilename "${dnaNormal_path}" \
-  --dnaTumorFilename "${dnaTumor_path}" \
+  $dnaTumor \
+  $dnaNormal \
+  $rnaTumor \
+  $rnaNormal \
   --fastaFilename "${fasta_path}" \
-  $options \
+  $radia_options \
   --outputDir ~/radiatemp/ \
   -o output.vcf \
   --scriptsDir ~/radia-1.1.5/scripts/ \
-  --patientId 20c31348-e871-4abb-8ec6-5124f8d0170e \
   --number_of_procs `nproc`
 mv output.vcf ~/out/output_vcf/output.radia.vcf
 cd ..
@@ -31,20 +80,13 @@ python ~/radia_filter.py \
   --dnaNormalFilename "${dnaNormal_path}" \
   --dnaTumorFilename "${dnaTumor_path}" \
   --fastaFilename "${fasta_path}" \
+  $radia_filter_options \
   --outputDir ~/radiafiltertemp/ \
   -o filtered.vcf \
   --scriptsDir /home/dnanexus/radia-1.1.5/scripts/ \
-  --patientId 20c31348-e871-4abb-8ec6-5124f8d0170e \
-  --number_of_procs `nproc` \
-  --makeTCGAcompliant \
-  --filter-reject \
-  --filter-germline
-
+  --number_of_procs `nproc`
 mv filtered.vcf ~/out/filtered_output_vcf/filtered.radia.vcf
 cd ..
-
-mkdir -p out/pass_filtered_output_vcf
-egrep "^\#|PASS" ~/out/filtered_output_vcf/filtered.radia.vcf > ~/out/pass_filtered_output_vcf/pass_filtered.radia.vcf
 
 dx-upload-all-outputs
 
